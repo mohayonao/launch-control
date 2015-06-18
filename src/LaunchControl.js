c1492597979f1e2e54357644f88e094a5e963b3f
mohayonao <mohayonao@gmail.com>
2015-06-19 07:27:13 +0900

    use @mohayonao/midi-device

---
+++ src/LaunchControl.js
@@ -0,0 +1,50 @@
+import xtend from "xtend";
+
+function parseMessage(b0, b1, b2) {
+  let value = Math.max(0, Math.min(b2, 127));
+  let channel = Math.max(1, Math.min((b0 & 0x0f) - 7, 16));
+
+  switch (b0 & 0xf0) {
+    case 0x90: // note on
+      if (0x09 <= b1 && b1 <= 0x0c && value === 127) {
+        return { control: "pad", track: (b1 - 0x08), value, channel };
+      }
+      if (0x19 <= b1 && b1 <= 0x1c) {
+        return { control: "pad", track: (b1 - 0x14), value, channel };
+      }
+      break;
+    case 0xb0: // control change
+      if (0x15 <= b1 && b1 <= 0x1c) {
+        return { control: "knob1", track: (b1 - 0x14), value, channel };
+      }
+      if (0x29 <= b1 && b1 <= 0x30) {
+        return { control: "knob2", track: (b1 - 0x28), value, channel };
+      }
+      break;
+  }
+
+  return null;
+}
+
+function _extends(MIDIDevice) {
+  return class LaunchControl extends MIDIDevice {
+    constructor(deviceName = "Launch Control") {
+      super(deviceName);
+
+      this._onmidimessage = (e) => {
+        let msg = parseMessage(e.data[0], e.data[1], e.data[2]);
+
+        if (msg === null) {
+          return;
+        }
+
+        this.emit("message", xtend({ type: "message" }, msg));
+      };
+    }
+  };
+}
+
+export default {
+  extends: _extends,
+  parseMessage,
+};
---
+++ src/WebMIDILaunchControl.js
@@ -0,0 +1,4 @@
+import WebMIDIDevice from "@mohayonao/midi-device/webmidi";
+import LaunchControl from "./LaunchControl";
+
+export default LaunchControl.extends(WebMIDIDevice);
