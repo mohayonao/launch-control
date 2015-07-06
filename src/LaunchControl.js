ef93f8973b66df3e810c6bbc01b6e978580245e4
mohayonao <mohayonao@gmail.com>
2015-07-06 14:54:45 +0900

    change event attribute: control -> dataType

--- src/LaunchControl.js
+++ src/LaunchControl.js
@@ -4,10 +4,10 @@ const PAD = [ 0x09, 0x0a, 0x0b, 0x0c, 0x19, 0x1a, 0x1b, 0x1c ];
 const KNOB1 = [ 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c ];
 const KNOB2 = [ 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30 ];
 const CURSOR = {
-  [0x72]: "up",
-  [0x73]: "down",
-  [0x74]: "left",
-  [0x75]: "right",
+  [0x72]: "cursor:up",
+  [0x73]: "cursor:down",
+  [0x74]: "cursor:left",
+  [0x75]: "cursor:right",
 };
 const COLOR_NAMES = {
   off: 0,
@@ -37,7 +37,7 @@ function parseMessage(st, d1, d2) {
   if (messageType === 0x90) {
     track = PAD.indexOf(d1);
     if (track !== -1) {
-      return { control: "pad", track, value, channel };
+      return { dataType: "pad", track, value, channel };
     }
   }
 
@@ -45,18 +45,18 @@ function parseMessage(st, d1, d2) {
   if (messageType === 0xb0) {
     track = KNOB1.indexOf(d1);
     if (track !== -1) {
-      return { control: "knob1", track, value, channel };
+      return { dataType: "knob1", track, value, channel };
     }
 
     track = KNOB2.indexOf(d1);
     if (track !== -1) {
-      return { control: "knob2", track, value, channel };
+      return { dataType: "knob2", track, value, channel };
     }
 
     let cursor = CURSOR[d1];
 
     if (cursor) {
-      return { control: `cursor:${cursor}`, value, channel };
+      return { dataType: cursor, value, channel };
     }
   }
 
